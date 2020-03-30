1fa8d68ecbed7263417a07617d980ad917aa1be3
陈蜜 <chenmi@sce-re.com>
2020-03-30 20:15:39 +0800

    download-git-repo 支持从 Github、Gitlab 下载远程仓库到本地

--- dist/utils/get.js
+++ dist/utils/get.js
@@ -1,1 +1,17 @@
 "use strict";
+import { getAll } from './rc';
+import downloadGit from 'download-git-repo';
+
+export const downloadLocal = async (templateName, projectName) => {
+    let config = await getAll();
+    let api = `${config.registry}/${templateName}`;
+    return new Promise((resolve, reject) => {
+        //projectName 为下载到的本地目录
+        downloadGit(api, projectName, (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
