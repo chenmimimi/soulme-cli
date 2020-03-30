5b13e24853c610f1a18a9c6247a9fb4c24cefc68
陈蜜 <chenmi@sce-re.com>
2020-03-30 20:53:52 +0800

    根据仓库的代码替换文件

--- dist/init.js
+++ dist/init.js
@@ -1,50 +1,65 @@
-"use strict";
+'use strict';
 
-import { downloadLocal } from './utils/get';
-import ora from 'ora';
-import inquirer from 'inquirer';
-import fs from 'fs';
-import chalk from 'chalk';
-import symbol from 'log-symbols';
+var _get = require('./utils/get');
+
+var _ora = require('ora');
+
+var _ora2 = _interopRequireDefault(_ora);
+
+var _inquirer = require('inquirer');
+
+var _inquirer2 = _interopRequireDefault(_inquirer);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _chalk = require('chalk');
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _logSymbols = require('log-symbols');
+
+var _logSymbols2 = _interopRequireDefault(_logSymbols);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 let init = async (templateName, projectName) => {
     //项目不存在
-    if (!fs.existsSync(projectName)) {
+    if (!_fs2.default.existsSync(projectName)) {
         //命令行交互
-        inquirer.prompt([
-            {
-                name: 'description',
-                message: 'Please enter the project description: '
-            },
-            {
-                name: 'author',
-                message: 'Please enter the author name: '
-            }
-        ]).then(async (answer) => {
+        _inquirer2.default.prompt([{
+            name: 'description',
+            message: 'Please enter the project description: '
+        }, {
+            name: 'author',
+            message: 'Please enter the author name: '
+        }]).then(async answer => {
             //下载模板 选择模板
             //通过配置文件，获取模板信息
-            let loading = ora('downloading template ...');
+            let loading = (0, _ora2.default)('downloading template ...');
             loading.start();
-            downloadLocal(templateName, projectName).then(() => {
+            (0, _get.downloadLocal)(templateName, projectName).then(() => {
                 loading.succeed();
                 const fileName = `${projectName}/package.json`;
-                if(fs.existsSync(fileName)){
-                    const data = fs.readFileSync(fileName).toString();
+                if (_fs2.default.existsSync(fileName)) {
+                    const data = _fs2.default.readFileSync(fileName).toString();
                     let json = JSON.parse(data);
                     json.name = projectName;
                     json.author = answer.author;
                     json.description = answer.description;
                     //修改项目文件夹中 package.json 文件
-                    fs.writeFileSync(fileName, JSON.stringify(json, null, '\t'), 'utf-8');
-                    console.log(symbol.success, chalk.green('Project initialization finished!'));
+                    _fs2.default.writeFileSync(fileName, JSON.stringify(json, null, '\t'), 'utf-8');
+                    console.log(_logSymbols2.default.success, _chalk2.default.green('Project initialization finished!'));
                 }
             }, () => {
                 loading.fail();
             });
         });
-    }else {
+    } else {
         //项目已经存在
-        console.log(symbol.error, chalk.red('The project already exists'));
+        console.log(_logSymbols2.default.error, _chalk2.default.red('The project already exists'));
     }
-}
+};
+
 module.exports = init;
