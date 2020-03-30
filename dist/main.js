6693d266eac225c6386e2edb911eee4a89c80636
陈蜜 <chenmi@sce-re.com>
2020-03-30 20:12:04 +0800

    利用 commander 来处理命令行

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,74 @@
-"use strict";
+import program from 'commander';
+import { VERSION } from './utils/constants';
+import apply from './index';
+import chalk from 'chalk';
+
+/**
+ * zero commands
+ *    - config
+ *    - init 
+ */
+
+let actionMap = {
+    init: {
+        description: 'generate a new project from a template',
+        usages: [
+            'zero init templateName projectName'
+        ]
+    },
+    config: {
+        alias: 'cfg',
+        description: 'config .zerorc',
+        usages: [
+            'zero config set <k> <v>',
+            'zero config get <k>',
+            'zero config remove <k>'
+        ]
+        
+    },
+    //other commands
+}
+
+// 添加 init / config 命令
+Object.keys(actionMap).forEach((action) => {
+    program.command(action)
+    .description(actionMap[action].description)
+    .alias(actionMap[action].alias) //别名
+    .action(() => {
+        switch (action) {
+            case 'config': 
+                //配置
+                apply(action, ...process.argv.slice(3));
+                break;
+            case 'init':
+                apply(action, ...process.argv.slice(3));
+                break;
+            default:
+                break;
+        }
+    });
+});
+
+function help() {
+    console.log('\r\nUsage:');
+    Object.keys(actionMap).forEach((action) => {
+        actionMap[action].usages.forEach(usage => {
+            console.log('  - ' + usage);
+        });
+    });
+    console.log('\r');
+}
+program.usage('<command> [options]');
+// zero -h 
+program.on('-h', help);
+program.on('--help', help);
+// zero -V   VERSION 为 package.json 中的版本号
+program.version(VERSION, '-V --version').parse(process.argv);
+
+// zero 不带参数时
+if (!process.argv.slice(2).length) {
+    program.outputHelp(make_green);
+}
+function make_green(txt) {
+    return chalk.green(txt); 
+}
